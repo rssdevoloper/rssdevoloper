3259a25230ae017f522c030b6f0e867e2bd17a8a
Rangga <65481692+rssdevoloper@users.noreply.github.com>
2024-05-05 08:57:49 +0700

    V1

---
+++ install_node.sh
@@ -0,0 +1,8 @@
+apt update
+apt upgrade
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+source ~/.bashrc
+nvm install 20
+nvm use 20
+echo "Nodejs Sudah terinstall"
